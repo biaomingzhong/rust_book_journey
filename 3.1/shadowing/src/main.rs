a6ca5e6bec83975fcbfbe4bd7b5f2234defa174d
quinlanzhong <quinlanzhong@meijiabang.cn>
2019-04-10 11:15:12 +0800

    feat: 完成 3.1

---
+++ 3.1/shadowing/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let x = 5;
+    println!("The value of x is: {}", x);
+    let x = x + 1;
+    println!("The value of x is: {}", x);
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+    
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("The length and value of spaces is: {}", spaces);
+    
+    let mut spaces = "   ";
+    spaces = spaces.len();
+    println!("The length and value of spaces is: {}", spaces);
+}
